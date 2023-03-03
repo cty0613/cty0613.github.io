13f35647f3ac0a91ea23ebf3ee9d8a7522886cfc
Raymond Choi <103353074+cty0613@users.noreply.github.com>
2023-03-03 21:35:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $('.songcell').click((e)=>{
 	var currentId = $(e.currentTarget).attr('id');
 	console.log(currentId);
-	$(`#${currentId}-detail`).css("opacity", "1").css("visibility", "visible");
+	$(`#${currentId}-detail`).css('opacity', '1').css("visibility", "visible");
 	
 })
 
 $('.song-detail').click(function(e){
 	var showedId = $(e.currentTarget).attr('id');
 	console.log(showedId);
-	$(`#${showedId}`).css("opacity", "0").css("visibility", "hidden");
+	$(`#${showedId}`).css('opacity', '0').css("visibility", "hidden");
 })
