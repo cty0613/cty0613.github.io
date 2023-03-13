ee2979fe55ed9051d798e23642a85e578cef5358
Raymond Choi <103353074+cty0613@users.noreply.github.com>
2023-03-13 11:23:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var easterfactor = 0;
 var boom = '../pages/easter.html';
+var kapay = "https://qr.kakaopay.com/281006011123554622002048";
 
 $('.songcell').click((e)=>{
 	var currentId = $(e.currentTarget).attr('id');
@@ -33,4 +34,9 @@ $('.footer').click(()=>{
 
 $('.navlist-item').click(()=> {
     $('.nav-extend').slideToggle('0.4');
+})
+
+$('.nav-donate').click(()=>{
+    window.alert("저희 뚜라미에 큰 도움 주셔서 감사합니다. 확인 버튼을 누르시면 카카오페이 송금으로 이동합니다.");
+    location.href = kapay;
 })
