1b0216c3379c145c6ff4b468b5a1cc3f0612175f
Raymond Choi <103353074+cty0613@users.noreply.github.com>
2023-08-24 01:51:36 +0900

    Add files via upload

--- tu2023spring/js/main.js
+++ tu2023spring/js/main.js
@@ -5,6 +5,7 @@ var kapay = "https://qr.kakaopay.com/281006011123554622002048";
 $('.songcell').click((e)=>{
 	var currentId = $(e.currentTarget).attr('id');
 	console.log(currentId);
+    console.log("opened")
 	$(`#${currentId}-detail`).css('opacity', '1').css("visibility", "visible");
 	
 })
@@ -12,6 +13,7 @@ $('.songcell').click((e)=>{
 $('.song-detail').click(function(e){
 	var showedId = $(e.currentTarget).attr('id');
 	console.log(showedId);
+    console.log("closed")
 	$(`#${showedId}`).css('opacity', '0').css("visibility", "hidden");
 })
 
